b4a3f1b70203d9ad2485aa0490c0f1b439350665
Bwijn <779805126@qq.com>
2020-03-28 18:13:48 +0800

    admin 管理后台添加模型 videoapp 迁移文件未修复

--- video/admin.py
+++ video/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Video, VideoName
+from .models import Video, VideoName, Episode, Type
 
 # Register your models here.
 admin.site.register(Video)
 admin.site.register(VideoName)
+admin.site.register(Episode)
+admin.site.register(Type)
